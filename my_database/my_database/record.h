abe587c20f3a7a5f5891dee85cd8c4b46c8cb11d
redjen8 <seokwoo2525@gmail.com>
2022-05-24 18:30:50 +0900

    20220524 jsw Feat : record 헤더 파일 업데이트

--- my_database/my_database/record.h
+++ my_database/my_database/record.h
@@ -1,2 +1,22 @@
 #pragma once
+#include <vector>
+#include <string>
+
 #define MAX_COLUMN_LEN 50
+
+typedef struct column_meta
+{
+	int num_fixed;
+	int num_variable;
+}column_meta;
+
+class record
+{
+private:
+	int null_bitmap;
+	std::vector<std::string>* fixed_len_column;
+	std::vector<std::string>* var_len_column;
+public:
+	record(std::vector<std::string>* input_fixed, std::vector<std::string>* input_variable, column_meta column_info);
+	void print_record();
+};
